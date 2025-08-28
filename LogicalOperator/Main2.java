64307d79edd31ebaca449cb3d193407fc953085f
Er-Anushka-Singh <anushkalucky11@gmail.com>
2025-08-29 04:18:30 +0530

    Add the files

---
+++ LogicalOperator/Main2.java
@@ -0,0 +1,10 @@
+class Main2{
+    public static void main(String[] args) {
+     boolean a = true;
+     boolean b = false;
+     boolean c = true;
+     boolean result = (a || b) && (b||c);
+        System.out.println(result);
+
+    }
+}
---
+++ LogicalOperator/Main4.java
@@ -0,0 +1,10 @@
+class Main4{
+    public static void main(String[] args) {
+     int a = 20;
+     int b = 30;
+        System.out.println(a++ >25 && b++ >20);
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
