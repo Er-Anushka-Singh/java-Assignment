a59161c0067c280dd8731bc687abde1352c0ee8b
Er-Anushka-Singh <anushkalucky11@gmail.com>
2025-09-07 03:37:41 +0530

    Add assig

---
+++ Composition_Aggreation/Aggreation/Car.java
@@ -0,0 +1,29 @@
+package Composition_Aggreation.Aggreation;
+
+public class Car {
+
+		String name ;
+		Engine e;
+	
+	Car(){
+		
+	}
+	
+	public void setEngine(Engine e) {
+		this.e=e;
+	}
+	
+	
+	
+	public void  openGate() {
+		System.out.println("Car OpenGate! .....");
+	}
+	public void closeGate() {
+		System.out.println("Car closeGate !......");
+	}
+	
+
+}
+
+  
+
---
+++ Composition_Aggreation/Aggreation/CarDriver.java
@@ -0,0 +1,41 @@
+package Composition_Aggreation.Aggreation;
+
+public class CarDriver {
+	public static void main(String[] args) {
+	
+		
+		// Setter Injection  Composition relation
+		Car c1= new Car();
+		c1.name="Tata";
+		c1.setEngine(new Engine("Tata Engine" ,234));
+		System.out.println(c1.name);
+		c1.openGate();
+		c1.closeGate();
+		System.out.println(c1.e.brand);
+		System.out.println(c1.e.hp);
+		
+		
+		System.out.println("<======================>");
+		
+		
+		// Setter Injection Aggregation method
+		Car c2 = new Car();
+		c2.name = "MG";
+	    Engine e2 = new Engine("MG Engine", 170);
+	    c2.e=e2;
+		System.out.println(c2.e.brand);
+		System.out.println(c2.e.hp);
+		System.out.println(c2.name);
+		System.out.println(c2.name.length());
+		System.out.println(c2.name.toLowerCase() );
+		
+		
+		
+	}
+
+	
+
+}
+
+  
+
