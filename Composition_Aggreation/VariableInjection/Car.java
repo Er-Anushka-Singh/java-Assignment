356f7a4af24520fb9d5f4642db6718f5332cb30b
Er-Anushka-Singh <anushkalucky11@gmail.com>
2025-09-07 03:59:28 +0530

    Add assigment

---
+++ Composition_Aggreation/VariableInjection/Car.java
@@ -0,0 +1,23 @@
+package Composition_Aggreation.VariableInjection;
+
+public class Car {
+
+		String name ;
+		Engine e;
+	
+	Car(){
+		
+	}
+		
+	
+	public void  openGate() {
+		System.out.println("Car OpenGate! .....");
+	}
+	public void closeGate() {
+		System.out.println("Car closeGate !......");
+	}
+	
+
+}
+
+
---
+++ Composition_Aggreation/VariableInjection/Engine.java
@@ -0,0 +1,25 @@
+package Composition_Aggreation.VariableInjection;
+
+public class Engine {
+
+		String brand ;
+		int hp;
+		
+		Engine(){
+			
+		}
+		Engine(String brand , int hp){
+			this.brand=brand;
+			this.hp=hp;
+		}
+		public void start() {
+			System.out.println("Engine Started !....");
+		}
+	    
+		public void stop() {
+			System.out.println("Engine Stopped !....");
+		}
+
+}
+
+
